12bbcd6a8edee4fb9dc183ec2f7bff9c62372cd1
LabeledZed <81475174+LabeledZed@users.noreply.github.com>
2022-02-27 20:52:20 +0200

    Source code for client

--- client.py
+++ client.py
@@ -9,7 +9,7 @@ import psutil
 
 win = Tk()
 win.config(bg="#1a1a1a")
-win.title("rChat Client")
+win.title("rChat Beta Client")
 win.resizable(False, False)
 
 
@@ -34,7 +34,7 @@ def cpr(p):
 
 
 # Choosing Nickname
-lbl0 = Label(win, text="rText Client v1.01", bg="#1a1a1a", fg="#8cb8ff", font=("Arial", 16))
+lbl0 = Label(win, text="rChat Beta Client v1.01", bg="#1a1a1a", fg="#8cb8ff", font=("Arial", 16))
 lbl = Label(win, text="Nickname:", bg="#1a1a1a", fg="#ffffff", font=("Arial", 13))
 ent = Entry(win, bg="#1a1a1a", fg="#ffffff", font=("Arial", 13))
 lbl2 = Label(win, text="Server IP (blank for default):", bg="#1a1a1a", fg="#ffffff", font=("Arial", 13))
